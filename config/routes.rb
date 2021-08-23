0e0d1a5301363f6c829f3d28125064926f8d2fc7
nishimototo <dkspm2436@gmail.com>
2021-08-23 06:49:26 +0000

    検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'searches/search'
   devise_for :users
   root "homes#top"
   get "home/about" => "homes#about"
+  get "search" => "searches#search"
+
   resources :users do
     resource :relationships, only: [:create, :destroy]
     get "follows", on: :member
